21ef0588b54bf74c15a0491955f7758d4065c6eb
Connor Hill <Bluedrake42@gmail.com>
2024-08-20 02:06:04 -0400

    Changes to launch parameters

--- launch.bat
+++ launch.bat
@@ -1,5 +1,4 @@
 @echo off
-:start
 
 echo Running update...
 call update.bat
@@ -9,12 +8,4 @@ timeout /t 5 /nobreak > nul
 
 start server\HarshDoorstop\Binaries\Win64\HarshDoorstopServer.exe AAS-TestMap?MaxPlayers=16 -log -SteamServerName="Harsh Doorstop Dedicated Server" %*
 
-echo Server started. Press any key to stop the server and update again.
-pause > nul
-
-echo Stopping server...
-taskkill /F /IM HarshDoorstopServer.exe
-timeout /t 10 /nobreak > nul
-
-echo Server stopped. Restarting process...
-goto start
+echo Server started.
